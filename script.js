fbef9334596a527908721a5b057df1251c279e8f
info1 <s@s.co>
2023-10-24 14:34:07 -0400

    Bootstrap ajouté

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-let formulaire = document.getElementById("form")
-formulaire.setAttribute("style", "background-color:yellow")
+let ajouts = document.getElementById("ajouts")
 
-let titre = document.getElementsByTagName("h1")[0]
-titre.innerHTML = "Formulaire <em>  Javascript</em> "
-
-let bouton = document.createElement("input")
+let bouton = document.getElementById("BoutonSubmit")
 bouton.setAttribute("type", "reset")
-formulaire.appendChild(bouton)
+
 
 // Fonction pour copier le contenu du formulaire au bas de la page
 function copier()
@@ -14,21 +10,21 @@ function copier()
     let nom = document.getElementById("nom").value
     let p = document.createElement("p") // Le p crée une balise parag
     p.innerText = nom
-    formulaire.appendChild(p)
+    ajouts.appendChild(p)
 
     let prenom = document.getElementById("prenom").value
     let pr = document.createElement("p")
     pr.innerText = prenom
-    formulaire.appendChild(pr)
+    ajouts.appendChild(pr)
 
     let email = document.getElementById("Email").value
     let ema = document.createElement("p")
     ema.innerText = email
-    formulaire.appendChild(ema)
+    ajouts.appendChild(ema)
 
     let telephone = document.getElementById("Telephone").value
     let tele = document.createElement("p")
     tele.innerText =  telephone
-    formulaire.appendChild(tele)
+    ajouts.appendChild(tele)
 
 }
